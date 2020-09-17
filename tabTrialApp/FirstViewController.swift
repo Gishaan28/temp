05af755227435efd4b302a3965a4fa8e64194e7b
Gishaan28 <post.ishaan@gmail.com>
2020-09-16 17:50:51 -0700

    First Commit

---
+++ tabTrialApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  tabTrialApp
+//
+//  Created by Ishaan Gupta 2 on 9/16/20.
+//  Copyright © 2020 Ishaan Gupta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ tabTrialApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabTrialApp
+//
+//  Created by Ishaan Gupta 2 on 9/16/20.
+//  Copyright © 2020 Ishaan Gupta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+        Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(updateUI), userInfo: nil, repeats: true)
+  }
+  
+  @objc func updateUI() {
+    print("yes \(self.navigationController?.visibleViewController)")
+    
+  }
+  
+
+}
+
